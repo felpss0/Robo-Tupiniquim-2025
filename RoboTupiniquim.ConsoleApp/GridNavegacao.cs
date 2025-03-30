c8eea81749eeeaf6f76657575e46d86e0bf4d70d
Filipe Straubel <fstraubel10@gmail.com>
2025-03-30 15:17:01 -0300

    Feito as classes do grid de naveção e do robo e feito o codigo base para input de dados pelo usuario

---
+++ RoboTupiniquim.ConsoleApp/GridNavegacao.cs
@@ -0,0 +1,20 @@
+﻿
+namespace RoboTupiniquim.ConsoleApp
+{
+    class GridNavegacao
+    {
+        public static int limiteX;
+        public static int limiteY;
+
+        public static void LimiteDaArea()
+        {
+            Console.WriteLine("Digite o limite da area Horizontal (X): ");
+            limiteX = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o limite da area Vertical (Y): ");
+            limiteY = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Area de navegação definida 0 a {limiteX} no eixo X e 0 a {limiteY} no eixo Y");
+        }
+    }
+}
---
+++ RoboTupiniquim.ConsoleApp/Robo.cs
@@ -0,0 +1,34 @@
+﻿
+
+namespace RoboTupiniquim.ConsoleApp
+{
+    class Robo
+    {
+        public static int posicaoX;
+        public static int posicaoY;
+        public static string direcao;
+
+        
+
+        public static void PosicaoInicialRobo() 
+        {
+            Console.WriteLine("Digite a posição inicial do robo");
+            Console.WriteLine("--------------------------------");
+            Console.Write("Posição X: ");
+            posicaoX = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Posição Y: ");
+            posicaoY = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o lado que o robo vai estar olhando (Norte,Sul,Leste,Oeste) ");
+            direcao = Convert.ToString(Console.ReadLine().ToUpper());
+
+
+        }
+
+        public static void StatusRobo()
+        {
+            Console.WriteLine($"Posição ({posicaoX},{posicaoY}), Direção: {direcao}");
+        }
+    }
+}
